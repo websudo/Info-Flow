e5602f546be55f1984f3aebc938ca4de740158a6
websudo <ritiknair19@gmail.com>
2021-08-15 18:48:57 +0530

    User auth created - not completed (Testing)

--- Backend/server.js
+++ Backend/server.js
@@ -2,20 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose')
 const mongodb = require('mongodb')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 dotenv.config()
 
+//import Routes
+const authRoutes = require('./routes/auth')
+
+// app 
 const app = express();
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+//Routes Middleware
+app.use( '/api' , authRoutes)
+
+
+// Database
 const MongoClient = mongodb.MongoClient
-const port = process.env.PORT || 8000
 const db = process.env.MONGODB_URI
 
-app.use(express.json())
+MongoClient.connect(db)
+.then( () => { console.log( "Database Connected")})
+.catch( err => { console.err })
 
-MongoClient.connect(db , () => {console.log("hello")})
-/*.then( () => { console.log( "Database Connected")})
-.catch( err => { console.err })*/
 
+
+const port = process.env.PORT || 5000
 app.listen( port , ()=>{
     console.log( `Server started at ${port}`)
 })
