f17bad70b64a8df47e2c42c4beade4b406f14742
Ritik Nair <ritiknair19@gmail.com>
2021-08-18 20:35:39 -0700

    Added background to the auth page

--- Frontend/src/pages/auth/Auth.js
+++ Frontend/src/pages/auth/Auth.js
@@ -8,7 +8,6 @@ import Signin from '../../components/signin/Signin'
 import Signup from '../../components/signup/SignUp'
 import Typography from '@material-ui/core/Typography';
 import {Link} from 'react-router-dom'
-import background from '../../assets/background/auth-background.jpg'
 import { height } from '@material-ui/system';
 import './Auth.css'
 
@@ -32,11 +31,23 @@ const useStyles = makeStyles((theme) => ({
 
     auth__main:{
         textAlign: 'center',
+        backgroundImage: 'url(https://images.unsplash.com/photo-1596638787647-904d822d751e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1462&q=80)',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        height: '100vh',
+        marginBottom: 0,
         
     },
 
     auth__logo: {
-        marginTop: 150,
+        
+        textDecoration: 'none',
+        color: 'white'
+    },
+
+    auth__logo__container:{
+        paddingTop: 40,
     }
   }));
 
@@ -67,8 +78,8 @@ export default function Auth() {
     return (
         <div className={classes.auth__main}>
 
-            <Link to='/'>
-                <Typography variant="h1" component="h2" gutterBottom className={classes.auth__logo}>
+            <Link to='/' className={classes.auth__logo}>
+                <Typography variant="h1" component="h2" gutterBottom className={classes.auth__logo__container}>
                     Info-Flow
                 </Typography>
             </Link>
