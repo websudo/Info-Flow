ede9ddc079119325b0cb4361deb35a3afff7738f
Ritik Nair <ritiknair19@gmail.com>
2022-06-14 17:44:34 +0530

    Offline page edited

--- Frontend/public/sw.js
+++ Frontend/public/sw.js
@@ -38,7 +38,7 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
-const OFFLINE_URL = './offline.html';
+const OFFLINE_URL = `${process.env.PUBLIC_URL}/offline.html`;
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
